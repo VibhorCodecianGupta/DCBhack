bcc6051bf8e9f2e55e548588dcddc775056688bd
VibhorCodecianGupta <VibhorCodecianGupta@users.noreply.github.com>
2017-09-10 10:48:37 +0530

    adhar aur blockchain ka scene

--- controller.js
+++ controller.js
@@ -6,8 +6,8 @@ router.get('/',function(req,res){
   res.send('hello');
 });
 
-var options = { method: 'POST',
-  url: 'https://preproduction.signzy.tech/api/v2/patrons/login',
+var options1 = { method: 'POST',
+  url: 'https://preproduction.signzy.tech/api/v2/patrons/login?access_token=9GAyXbUGgfnTR35iqqeqKS5HcSTX8lDlLSeQBYd4',
   headers:
    { accept: '*/*',
      'content-type': 'application/json',
@@ -15,33 +15,72 @@ var options = { method: 'POST',
   body: { username: 'dcbtest2', password: '9GAyXbUGgfnTR35iqqeqKS5HcSTX8lDlLSeQBYd4' },
   json: true };
 
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+  // request(options, function (error, response, body) {
+  //   if (error) throw new Error(error);
+  //
+  //   console.log(body);
+  // });
+  //var id = 'abc';
 
-    console.log(body);
-  });
+  // request(options, function (error, response, body) {
+  //   if (error) throw new Error(error);
+  //
+  //   console.log(body);
+  // });
+//var itemId = 'abc';
 
-  var options = { method: 'POST',
-    url: 'https://preproduction.signzy.tech/api/v2/patrons/59b2b774e2c05333a656fb5d/identities',
-    headers:
-     { authorization: '9GAyXbUGgfnTR35iqqeqKS5HcSTX8lDlLSeQBYd4',
-       accept: '*/*',
-       'content-type': 'application/json',
-       'accept-language': 'en-US,en;q=0.8' },
-    body:
-     { type: 'individualPan',
-       callbackUrl: 'https://localhost:3000',
-       email: 'vibhordelgupta@gmail.com',
-       images: [ 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Allenton_Hippo_QR_Code.jpg' ] },
-    json: true };
+// request(options, function (error, response, body) {
+//   if (error) throw new Error(error);
+//   console.log(error);
 
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+  request(options1, function(error, res, body){
+    if(error) throw new Error(error);
+    //userId = body.userId;
+    id = body.id;
+    console.log(id);
+    console.log('done 1');
 
-    console.log(body);
-  });
+    var options2 = { method: 'POST',
+      url: 'https://preproduction.signzy.tech/api/v2/patrons/59b2b774e2c05333a656fb5d/identities',
+      headers:
+       { authorization: '9GAyXbUGgfnTR35iqqeqKS5HcSTX8lDlLSeQBYd4',
+         Authorization: id,
+         accept: '*/*',
+         'content-type': 'application/json',
+         'accept-language': 'en-US,en;q=0.8' },
+      body:
+       { type: 'individualPan',
+         callbackUrl: 'https://localhost:3000',
+         email: 'vibhordelgupta@gmail.com',
+         images: [ 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Allenton_Hippo_QR_Code.jpg' ] },
+      json: true };
 
-  var options = { method: 'POST',
+
+    //console.log(body);
+    request(options2, function(error, res, body){
+      if(error) throw new Error(error);
+      var itemId = body.id;
+      var accessToken = body.accessToken;
+      console.log('done 2');
+      console.log(body);
+
+      // var options3 = { method: 'POST',
+      // url: 'https://preproduction.signzy.tech/api/v2/snoops',
+      // headers:
+      //  { accept: '*/*',
+      //    'content-type': 'application/json',
+      //    'accept-language': 'en-US,en;q=0.8',
+      //    //'access_token': 'buc1btmliiny33fcboy5pnwmilp37suo4zxi2cginu4ucy2e29k0834mu0c80uyxo4rt3xr'
+      //   },
+      // body:
+      //  { service: 'Identity',
+      //    itemId: itemId,
+      //    accessToken: accessToken,
+      //    task: 'decodeQR',
+      //    essentials: { url: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Allenton_Hippo_QR_Code.jpg' } },
+      // json: true };
+
+      var options3 = { method: 'POST',
   url: 'https://preproduction.signzy.tech/api/v2/snoops',
   headers:
    { accept: '*/*',
@@ -49,18 +88,26 @@ var options = { method: 'POST',
      'accept-language': 'en-US,en;q=0.8' },
   body:
    { service: 'Identity',
-     itemId: '59b466f2a7f6d833b89f57be',
-     accessToken: 'buc1btmliiny33fcboy5pnwmilp37suo4zxi2cginu4ucy2e29k0834mu0c80uyxo4rt3xr',
-     task: 'decodeQR',
-     essentials: { url: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Allenton_Hippo_QR_Code.jpg' } },
+     itemId: itemId,
+     accessToken: accessToken,
+     task: 'verification',
+     essentials:
+      { uid: '868949908204',
+        name: 'Nikhil Kumar Mishra',
+        yob: '1997',
+        pincode: '110044' } },
   json: true };
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  console.log(error);
+      request(options3, function(error, res, body){
+        if(error) throw new Error(error);
+        console.log('done 3');
+        console.log(body);
+      });
+    });
+  });
 
   //console.log(body);
-});
+//});
 
 //id : 4mSio8TzFjyZZL72VTpIld8VFG8xf1CxBrFFiA6u7UY3fJwikyUuNq9ogFk9mcpA
 //"accessToken": "buc1btmliiny33fcboy5pnwmilp37suo4zxi2cginu4ucy2e29k0834mu0c80uyxo4rt3xr"
